8a5a6e4e681aee737b45898c5e4fff9d1371efb0
Paintface07 <paintface07@gmail.com>
2015-08-04 22:15:15 -0500

    add endpoint for item categories

--- eve.angular.app/src/main/java/eve/angular/app/controllers/ItemsController.java
+++ eve.angular.app/src/main/java/eve/angular/app/controllers/ItemsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 import eve.angular.app.config.EveApiConfiguration;
 import eve.angular.app.model.crest.EveCrestApi;
+import eve.angular.app.model.crest.items.CrestItemCategoryPage;
 import eve.angular.app.model.crest.items.CrestItemGroupPage;
 import eve.angular.app.model.crest.items.CrestItemTypePage;
 
@@ -36,6 +37,23 @@ public class ItemsController {
 		try {
 			EveCrestApi api = new RestTemplate().getForObject(EveApiConfiguration.BASE_URL, EveCrestApi.class);
 			CrestItemTypePage itemTypes = new RestTemplate().getForObject(api.getItemTypes().getHref(), CrestItemTypePage.class);
+			System.out.println(">>> Response: " + itemTypes);
+		
+			return itemTypes;
+		} catch(RestClientException ex) {
+			System.out.println("*** Could not reach EVE API server.  " + ex.getMessage());
+			return null;
+		} catch(Exception ex) {
+			System.out.println("*** Exception occurred: " + ex.getMessage());
+			return null;
+		}
+	}
+	
+	@RequestMapping(value="/inventory/categories", method=RequestMethod.GET, produces="application/json")
+	public CrestItemCategoryPage itemCategories() {
+		try {
+			EveCrestApi api = new RestTemplate().getForObject(EveApiConfiguration.BASE_URL, EveCrestApi.class);
+			CrestItemCategoryPage itemTypes = new RestTemplate().getForObject(api.getItemCategories().getHref(), CrestItemCategoryPage.class);
 			System.out.println(">>> Response: " + itemTypes);
 		
 			return itemTypes;
---
+++ eve.angular.app/src/main/java/eve/angular/app/model/crest/items/CrestItemCategory.java
@@ -0,0 +1,13 @@
+package eve.angular.app.model.crest.items;
+
+import java.io.Serializable;
+
+import eve.angular.app.model.crest.EveApiLink;
+import lombok.Data;
+
+@Data	// TODO: resolve this issue by implementing proper equals/hashcode
+public class CrestItemCategory extends EveApiLink implements Serializable {
+	private static final long serialVersionUID = 7103803713775899514L;
+	
+	private String name;
+}
